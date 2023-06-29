3f1823daafcd8fff6b76b09666b37c455e629568
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-29 14:50:45 -0300

    Teste de URL Restrita

--- src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -55,5 +55,11 @@ public class LoginTest {
         Assert.assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("usuario-logado")));
     }
 
-    
+    @Test
+    public void naoDeveriaAcessarPaginaRestritaSemEstarLogado(){
+        this.browser.navigate().to("http://localhost:8080/leiloes/2");
+        Assert.assertTrue(browser.getCurrentUrl().equals("http://localhost:8080/login"));
+        Assert.assertFalse(browser.getPageSource().contains("Dados do Leilão"));
+
+    }
 }
